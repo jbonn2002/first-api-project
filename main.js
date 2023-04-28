6078c52ec759cc3e90928e25ce6bd7ba55723ba7
Jordy <jbonn2002@gmail.com>
2023-04-27 19:30:59 -0700

    added some code to mainjs

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('button').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+    // const name = document.querySelector('input').value
+    // const res = await fetch(`/api?:name =${name}`)
+    // const data = await res.json()
+
+    console.log(data)
+    // document.querySelector('h2').textContent = data.birthName
+}
